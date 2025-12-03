d0f0a1a5b3374007bef0384f717f9ab493869b22
Max Carlquist <maxcarlquist@icloud.com>
2025-12-03 02:48:49 +0100

    fix: conditional rendering

--- app/admin/company/page.tsx
+++ app/admin/company/page.tsx
@@ -3,49 +3,43 @@
 import { DataTable } from "@/components/admin/data-table";
 import EmptyArea from "@/components/admin/empty-area";
 import { Suspense, useState, useEffect } from "react";
-import { set } from "react-hook-form";
 
 
 export default function CompanyAdminPage() {
     const [companies, setCompanies] = useState([]);
 
     useEffect(() => {
+        const fetchCompanies = async () => {
+            try {
+                const response = await fetch('/api/admin/company');
+                const data = await response.json();
+                setCompanies(data.companies);
+                console.log('companies', data.companies);
+                
+            } catch (error) {
+                console.error('Error fetching companies:', error);
+            }
+        };
+
         fetchCompanies();
-        
     }, []);
 
-    const fetchCompanies = async () => {
-        const response = await fetch('/api/admin/company');
-        const data = await response.json();
-        return data.companies;
-    };
-    
-    fetchCompanies()
-    .then(data => {
-        setCompanies(data);
-    })
-    .catch(error => {
-        console.error('Error fetching companies:', error);
-    });
 
-    
-    
     return (
         <>
-            {/* If there are no companies */}
-            
-            <EmptyArea title="No Companies Yet"
-                description={"You haven\'t created any companies yet. Get started by creating your first company."}
-                cta="Create Company"
-            />
-            {/* If there are companies, show the data table */}
-            
+            { companies.length > 0 ? (
+                <Suspense fallback={<div>Loading...</div>}>
+                    <DataTable />
+                </Suspense>
                 
-            <Suspense fallback={<div>Loading...</div>}>
-                <DataTable
-                    
+            ) : (
+                <EmptyArea title="No Companies Yet"
+                    description={"You haven\'t created any companies yet. Get started by creating your first company."}
+                    cta="Create Company"
                 />
-            </Suspense> 
+            )}
+
+            {/* If there are companies, show the data table */}
         </>
     );
 }

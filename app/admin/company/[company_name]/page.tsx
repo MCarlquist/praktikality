88680bb8bc912efcf427b3ba26e2826867241e0d
Max Carlquist <maxcarlquist@icloud.com>
2025-12-03 14:28:06 +0100

    adding

--- app/admin/company/[company_name]/page.tsx
+++ app/admin/company/[company_name]/page.tsx
@@ -1,9 +1,10 @@
 
 
-export default async function CompanyDetailPage ( { params }: { params: { company_name: string } } ) {
+export default async function CompanyDetailPage(props: { params: Promise<{ company_name: string }> }) {
+    const params = await props.params;
     const { company_name } = params;
     console.log('company', company_name);
-    
+
     return (
         <div>
             Company Detail Page for
--- app/auth/error/page.tsx
+++ app/auth/error/page.tsx
@@ -23,11 +23,12 @@ async function ErrorContent({
   );
 }
 
-export default function Page({
-  searchParams,
-}: {
-  searchParams: Promise<{ error: string }>;
-}) {
+export default async function Page(
+  props: {
+    searchParams: Promise<{ error: string }>;
+  }
+) {
+  const searchParams = await props.searchParams;
   return (
     <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm">

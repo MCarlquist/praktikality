92b8766c967fb7002f418944c49f7bf93c1c2b92
Max Carlquist <maxcarlquist@icloud.com>
2025-12-05 08:33:33 +0100

    fuck cache

--- app/admin/company/[company_name]/page.tsx
+++ app/admin/company/[company_name]/page.tsx
@@ -1,12 +1,9 @@
 import { Suspense } from "react";
 import CompanyDetailContent from "./company-detail-content-page";
-import { cacheLife } from "next/cache";
 
 
 
 export default async function CompanyDetailPage(props: { params: Promise<{ company_name: string }> }) {
-    "use cache"
-    cacheLife('default');
     const params = await props.params;
     const company_name = decodeURIComponent(params.company_name);
 

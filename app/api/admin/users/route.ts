0f70dc10e3ad4509e1ba8b45a1d12da0b07810a5
Max Carlquist <maxcarlquist@icloud.com>
2025-12-07 00:59:26 +0100

    working on deleting user

--- app/api/admin/users/route.ts
+++ app/api/admin/users/route.ts
@@ -2,11 +2,13 @@
 
 import { createServerSupabaseClient } from "@/lib/supabase/server";
 import { NextResponse } from "next/server";
+import type { NextRequest } from 'next/server';
+
 
 export async function GET() {
     try {
         const supabase = await createServerSupabaseClient();
-        let { data: users, error } = await supabase.from('profiles').select('*');
+        let { data: users, error } = await supabase.from('profiles').select('*').eq('role', 'user');
 
         return NextResponse.json({ users });
     } catch (error) {
@@ -15,15 +17,27 @@ export async function GET() {
     }
 }
 
-export async function POST() {
+
+export async function DELETE(req: NextRequest) {
+    console.log('deleting user');
+
+    const fromServer = req.nextUrl.searchParams.get('user');
+    console.log(fromServer);
+
     try {
         const supabase = await createServerSupabaseClient();
-        let { data: users, error } = await supabase.from('profiles').select('*');
 
-        return NextResponse.json({ users });
+        const { error } = await supabase
+            .from('profiles')
+            .delete()
+            .eq('id', fromServer)
+        if (error) {
+            throw new Error(error.message);
+        }
+        return NextResponse.json({ message: 'User deleted successfully', success: true });
     } catch (error) {
         console.log('error', error);
-        return NextResponse.json({ error: 'Failed to fetch users' }, { status: 500 });
+        return NextResponse.json({ error: 'Failed to delete user', success: false }, { status: 500 });
     }
 }
 
--- components/admin/users/data-table/columns.tsx
+++ components/admin/users/data-table/columns.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import { useState } from "react";
 import { ColumnDef } from "@tanstack/react-table";
 import { MoreHorizontal } from "lucide-react";
 import { Switch } from "@/components/ui/switch"
@@ -17,16 +17,33 @@ import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, D
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 
-// This type is used to define the shape of our data.
-// You can use a Zod schema here if you want.
+
 export type UserTableData = {
     email: string,
-    role: string
 }
 
 function ActionsCell({ row }: { row: any }) {
-    const [open, setOpen] = React.useState(false);
+    const [open, setOpen] = useState(false);
+    const [users, setUsers] = useState<any[]>([]);
 
+    const { want_internship, email, companies_they_work_at } = row.original;
+
+
+    const deleteUser = async () => {
+        const response = await fetch(`/api/admin/users?user=${row.original.id}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+        const data = await response.json();
+        if (data.success) {
+            setUsers(users.filter(user => user.id !== row.original.id));
+        }
+        
+        setUsers(users.filter(user => user.id !== row.original.id));
+    }
+    
     return (
         <>
             <DropdownMenu>
@@ -39,25 +56,29 @@ function ActionsCell({ row }: { row: any }) {
                 <DropdownMenuContent align="end">
                     <DropdownMenuLabel>Actions</DropdownMenuLabel>
                     <DropdownMenuItem onClick={() => setOpen(true)}>
-                        Edit profile
+                        View user
                     </DropdownMenuItem>
                     <DropdownMenuSeparator />
-                    <DropdownMenuItem className="text-destructive">Delete</DropdownMenuItem>
+                    <DropdownMenuItem className="text-destructive" onClick={deleteUser}>Delete User</DropdownMenuItem>
                 </DropdownMenuContent>
             </DropdownMenu>
 
             <Dialog open={open} onOpenChange={setOpen}>
                 <DialogContent className="sm:max-w-[425px]">
                     <DialogHeader>
-                        <DialogTitle>Change Role</DialogTitle>
+                        <DialogTitle>Edit User</DialogTitle>
                         <DialogDescription>
-                            Make this user admin.
+                            Make changes to { email }
                         </DialogDescription>
                     </DialogHeader>
                     <form onSubmit={(e) => e.preventDefault()}>
                         <div className="grid gap-4">
-                            <Label htmlFor="role">Change role to admin</Label>
-                            <Switch id="id" />
+                            <Label>Do they want to find internship?</Label>
+                            {want_internship ? "Yes" : "No"}
+                        </div>
+                        <div>
+                            <Label>Companies they work at</Label>
+                            {companies_they_work_at}
                         </div>
                         <DialogFooter>
                             <Button variant="destructive" onClick={() => setOpen(false)}>No</Button>
@@ -74,10 +95,6 @@ export const userColumns: ColumnDef<UserTableData>[] = [
     {
         accessorKey: "email",
         header: "Email",
-    },
-    {
-        accessorKey: "role",
-        header: "Role",
     },
     {
         id: "actions",

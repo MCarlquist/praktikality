7f831ab69bb68fd87d72f29c88097c89c91b9117
Max Carlquist <tenkaklet@gmail.com>
2026-08-29 21:37:19 +0200

    adding workflow and begun profile

--- app/company/[company_name]/company-detail-content-page.tsx
+++ app/company/[company_name]/company-detail-content-page.tsx
@@ -3,6 +3,8 @@ import { useEffect, useState } from "react";
 import { Checkbox } from "@/components/ui/checkbox";
 import { SquareMousePointer } from "lucide-react";
 import { Spinner } from "@/components/ui/spinner";
+import { Button } from "@/components/ui/button";
+import LanguagesBadges from "@/components/company/languages_badges";
 
 type User = {
     id: string,
@@ -39,8 +41,9 @@ export default function CompanyDetailContent({ companyName }: { companyName: str
                 setContact(result.company.company_contact);
                 setWebsite(result.company.company_site);
 
+                // TODO: fetch signed up users from database.
                 // Creating sample Array of users at company
-                const userTestDataArray: User[] = [{id: 'efcid83', email: 'user1@codex.com'}, {id: 'fdak382', email: 'user2@codex.com'}];
+                const userTestDataArray: User[] = [{ id: 'efcid83', email: 'user1@codex.com' }, { id: 'fdak382', email: 'user2@codex.com' }];
                 setUsers(userTestDataArray);
             } catch (err) {
                 setError(err instanceof Error ? err.message : "Error fetching data");
@@ -54,32 +57,44 @@ export default function CompanyDetailContent({ companyName }: { companyName: str
         }
     }, [companyName]);
 
+    // TODO: User is interested in a company, add that logic.
+    // User wants to be a intern at this company.
+    const joinCompany = async () => {
+        alert(companyName);
+    }
+
     if (loading) return <div className="flex flex-col"><Spinner className="size-8" /> Loading Company...</div>;
     if (error) return <div>Error: {error}</div>;
 
     return (
-        <div className="mx-auto">
-            <p className="text-5xl mb-3">{name}</p>
-            <p>Company Type: {type}</p>
-            <p>Company Size: {size} people</p>
-            <p>Do they already have an intern? Yes <Checkbox checked={haveIntern === 'yes' ? true : false} /> No <Checkbox checked={haveIntern === 'no' ? true : false} /></p>
-            <p>Programming Languages:</p>
-            <ul className="text-md">{programmingLanguages.map((lang, index) => (
-                <li key={index}>
-                    <p className="text-sm">-{lang}</p>
-                </li>
-            ))}</ul>
-            <p>Is it remote? Yes <Checkbox checked={remote === 'yes' ? true : false} /> No <Checkbox checked={remote === 'no' ? true : false} /></p>
-            <p>Location: {location}</p>
-            <p>Contact: <a className="text-blue-400" href={`mailto:${contact}`}>{contact}</a></p>
-            <p>Company website: {website != null ? <a className="flex gap-1" href={website} target="_blank">{website} <SquareMousePointer className="size-4" /></a> : 'not supplied'}</p>
+        <div className="mx-auto flex flex-auto gap-5">
+            <div>
+                <p className="text-5xl mb-3">{name}</p>
+                <p>Company Type: {type}</p>
+                <p>Company Size: {size} people</p>
+                <p>Do they already have an intern? Yes <Checkbox checked={haveIntern === 'yes' ? true : false} /> No <Checkbox checked={haveIntern === 'no' ? true : false} /></p>
+                
+                <LanguagesBadges languages={programmingLanguages} />
+                
+                <p>Is it remote? Yes <Checkbox checked={remote === 'yes' ? true : false} /> No <Checkbox checked={remote === 'no' ? true : false} /></p>
+                <p>Location: {location}</p>
+                <p>Contact: <a className="text-blue-400" href={`mailto:${contact}`}>{contact}</a></p>
+                <p>Company website: {website != null ? <a className="flex gap-1" href={website} target="_blank">{website} <SquareMousePointer className="size-4" /></a> : 'not supplied'}</p>
 
-            <div className="mt-4">
-                <p>Current Deltagare:</p>
-                <small>{
-                    users.map(((user) =>
-                        <p key={user.id}>{user.email}</p> 
-                    ))}</small>
+                <div className="mt-4">
+                    <p>Current Deltagare:</p>
+                    <small>{
+                        users.map(((user) =>
+                            <p key={user.id}>{user.email}</p>
+                        ))}</small>
+                </div>
+            </div>
+            <div className="flex flex-col text-center gap-3">
+                <h3 className="text-2xl">Vill du ha praktik hos dom?</h3>
+                <p>Intresserad?</p>
+                <Button onClick={joinCompany}>
+                    Ja, Gärna
+                </Button>
             </div>
         </div>
     );

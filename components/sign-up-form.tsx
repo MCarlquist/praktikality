75fa617e92cd65a3f467c6975e2721c3f543d318
Max Carlquist <tenkaklet@gmail.com>
2026-09-13 21:57:11 +0200

    feat: add user name to sign-up options and enable navigation to success page

--- components/sign-up-form.tsx
+++ components/sign-up-form.tsx
@@ -58,6 +58,9 @@ export function SignUpForm({
         password,
         options: {
           emailRedirectTo: `${window.location.origin}/directory`,
+          data: {
+            name,
+          },
         },
       });
 
@@ -77,7 +80,7 @@ export function SignUpForm({
 
 
 
-      //router.push("/auth/sign-up-success");
+      router.push("/auth/sign-up-success");
     } catch (error: unknown) {
       setError(error instanceof Error ? error.message : "Sorry , something went wrong");
     } finally {

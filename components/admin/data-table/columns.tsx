cb8ca824ecf605acde8d9ecec5effcf436ed6742
MCarlquist <tenkaklet@gmail.com>
2026-09-20 12:09:00 +0200

    fix: amount of users shown (#49)
    * fix: amount of users shown
    
    * hotfix: removed console.log

--- components/admin/data-table/columns.tsx
+++ components/admin/data-table/columns.tsx
@@ -47,12 +47,17 @@ export const columns: ColumnDef<CompanyTableData>[] = [
     header: 'Antal på praktik',
     cell: ({ row }) => {
       const deltagare = row.original.deltagare;
-      return deltagare ? deltagare.length : '-';
+      return deltagare ? deltagare.length : '0';
     }
   },
   {
     accessorKey: 'antal_intresserade',
-    header: 'Antal intresserade'
+    header: 'Antal intresserade',
+    cell: ({ row }) => {
+      const howMany = row.original.antal_intresserade;
+      return howMany ? howMany.length : '0';
+      
+    },
   },
   {
     id: "actions",
